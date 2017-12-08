232369340b86e985eec296dbe1e18b982b8cb9cb
zhangjams <wzhang76@illinois.edu>
2017-12-08 14:39:12 -0500

    ip trying

--- src/app.py
+++ src/app.py
@@ -123,5 +123,5 @@ def upload_data():
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # socketio.run(app, host='0.0.0.0', port=5000, debug=True)
